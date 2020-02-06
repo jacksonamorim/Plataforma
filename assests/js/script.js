a2cc0471107bf242027ba4d444dd544fd218a405
Jackson Amorim <jacksonamorim1995@gmail.com>
2020-02-06 18:27:18 -0300

    Adicionando todos os arquivos no GITHUB

---
+++ assests/js/script.js
@@ -0,0 +1,30 @@
+const s = (el) => document.querySelector(el);
+const curso = document.querySelectorAll('.section-course--cursos');
+
+s('.aside-buttom').addEventListener('click', ()=>{
+    let menu = s('.menu');
+    if(menu.style.width == '200px'){
+        menu.style.width = '70px';
+    }else{
+        menu.style.width = '200px';
+    }
+});
+curso.forEach((curso) => {
+    s('.option-light').addEventListener('click', () => {
+        s('aside').classList.add('aside-color');
+        s('.menu').classList.add('menu-color');
+        s('.conteudo').classList.add('conteudo-color');
+        s('.section-categories').classList.add('section-categories-color');
+        s('footer').classList.add('footer-color');
+        curso.classList.add('section-course--color');
+    });
+
+    s('.option-dark').addEventListener('click', () => {
+        s('aside').classList.remove('aside-color');
+        s('.menu').classList.remove('menu-color');
+        s('.conteudo').classList.remove('conteudo-color');
+        s('.section-categories').classList.remove('section-categories-color');
+        s('footer').classList.remove('footer-color');
+        curso.classList.remove('section-course--color');
+    });
+});
