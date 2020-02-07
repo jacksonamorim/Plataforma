4325ac86bb058ed6c14ec8ea1885aa6c9feb7a8e
Jackson Amorim <jacksonamorim1995@gmail.com>
2020-02-07 18:43:58 -0300

    Sessão Login construida

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,4 +1,5 @@
 const s = (el) => document.querySelector(el);
+const sa = (el) => document.querySelectorAll(el);
 const curso = document.querySelectorAll('.section-course--cursos');
 
 s('.aside-buttom').addEventListener('click', ()=>{
@@ -9,22 +10,34 @@ s('.aside-buttom').addEventListener('click', ()=>{
         menu.style.width = '200px';
     }
 });
-curso.forEach((curso) => {
-    s('.option-light').addEventListener('click', () => {
-        s('aside').classList.add('aside-color');
-        s('.menu').classList.add('menu-color');
-        s('.conteudo').classList.add('conteudo-color');
-        s('.section-categories').classList.add('section-categories-color');
-        s('footer').classList.add('footer-color');
+
+s('.option-light').addEventListener('click', () => {
+    s('aside').classList.add('aside-color');
+    s('.menu').classList.add('menu-color');
+    s('.conteudo').classList.add('conteudo-color');
+    s('.section-categories').classList.add('section-categories-color');
+    s('footer').classList.add('footer-color');
+    curso.forEach((curso) => {
         curso.classList.add('section-course--color');
     });
-
-    s('.option-dark').addEventListener('click', () => {
-        s('aside').classList.remove('aside-color');
-        s('.menu').classList.remove('menu-color');
-        s('.conteudo').classList.remove('conteudo-color');
-        s('.section-categories').classList.remove('section-categories-color');
-        s('footer').classList.remove('footer-color');
-        curso.classList.remove('section-course--color');
-    });
 });
+
+s('.option-dark').addEventListener('click', () => {
+    s('aside').classList.remove('aside-color');
+    s('.menu').classList.remove('menu-color');
+    s('.conteudo').classList.remove('conteudo-color');
+    s('.section-categories').classList.remove('section-categories-color');
+    s('footer').classList.remove('footer-color');
+    curso.forEach((curso) => {
+        curso.classList.remove('section-course--color');
+    })
+});
+
+s('.option-light').addEventListener('click', ()=>{
+    s('.section-signin').classList.add('signin-color');
+    s('.section-login').classList.add('login-color');
+})
+s('.option-dark').addEventListener('click', ()=>{
+    s('.section-signin').classList.remove('signin-color');
+    s('.section-login').classList.remove('login-color');
+})
